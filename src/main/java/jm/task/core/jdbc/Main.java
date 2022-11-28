989212b60576bf6028cfd71663983b365a680179
DmitriyS <eastmoscow@mail.ru>
2022-11-28 17:53:49 +0300

    PP_z115

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,10 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
